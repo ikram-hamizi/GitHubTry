529c58a09b64d221dc3c8d2a3414b6007ac102ad
Minh Nguyet Nguyen <nguyenmnt@vcu.edu>
2018-03-24 15:25:32 -0400

    button coding and managing user entries

--- app/src/main/java/com/example/usp05/githubtry/MainActivity.java
+++ app/src/main/java/com/example/usp05/githubtry/MainActivity.java
@@ -11,6 +11,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onLoginClick(View view) {
+
+    }
+
     public void onSignUpClick(View view) {
         if(view.getId() == R.id.signUpButton) {
             Intent i = new Intent(MainActivity.this, Register.class);
