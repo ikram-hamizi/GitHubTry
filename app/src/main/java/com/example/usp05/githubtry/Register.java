60bc3ccdd933905e84aa0f32a1550b1f35f57e5d
Minh Nguyet Nguyen <nguyenmnt@vcu.edu>
2018-03-24 15:25:32 -0400

    button coding and managing user entries

--- app/src/main/java/com/example/usp05/githubtry/Register.java
+++ app/src/main/java/com/example/usp05/githubtry/Register.java
@@ -1,7 +1,11 @@
 package com.example.usp05.githubtry;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * Created by minh on 3/24/18.
@@ -13,4 +17,38 @@ public class Register extends Activity {
         setContentView(R.layout.register);
     }
 
+    public void onRegisterClick(View view) {
+        if(view.getId() == R.id.registerButton) {
+            EditText username = (EditText)findViewById(R.id.usernameRegister);
+            EditText password = (EditText)findViewById(R.id.passwordRegister);
+            EditText password2 = (EditText)findViewById(R.id.password2Register);
+            EditText secQuestion1 = (EditText)findViewById(R.id.secQuestion1);
+            EditText secQuestion2 = (EditText)findViewById(R.id.secQuestion2);
+            EditText secQuestion3 = (EditText)findViewById(R.id.secQuestion3);
+
+            String usernameStr = username.getText().toString();
+            String passwordStr = password.getText().toString();
+            String password2Str = password2.getText().toString();
+            String secQuestion1Str = secQuestion1.getText().toString();
+            String secQuestion2Str = secQuestion2.getText().toString();
+            String secQuestion3Str = secQuestion3.getText().toString();
+
+            if(usernameStr.equals("") || password.equals("") || password2.equals("") ||
+                    secQuestion1Str.equals("") || secQuestion2Str.equals("") || secQuestion3Str.equals("")) {
+                Toast message = Toast.makeText(Register.this, "Entries cannot be empty!", Toast.LENGTH_SHORT);
+                message.show();
+            }
+            else {
+                if (passwordStr.equals(password2Str)) {
+                    // insert details into database
+                    Intent i = new Intent(Register.this, MainActivity.class);
+                    startActivity(i);
+                }
+                else {
+                    Toast message = Toast.makeText(Register.this, "Passwords are not the same!", Toast.LENGTH_SHORT);
+                    message.show();
+                }
+            }
+        }
+    }
 }
