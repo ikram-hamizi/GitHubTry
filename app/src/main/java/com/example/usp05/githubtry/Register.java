adf3606916469abf25b61e3efb1cd47c329fa4b2
Minh Nguyet Nguyen <nguyenmnt@vcu.edu>
2018-03-24 14:32:39 -0400

    adding login and register page

---
+++ app/src/main/java/com/example/usp05/githubtry/Register.java
@@ -0,0 +1,16 @@
+package com.example.usp05.githubtry;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by minh on 3/24/18.
+ */
+
+public class Register extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
+    }
+
+}
