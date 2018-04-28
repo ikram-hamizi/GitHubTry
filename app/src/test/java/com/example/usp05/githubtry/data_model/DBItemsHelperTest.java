1f3e887306ce930ca817eadc137f349b32b2cae9
Nathan Puryear <puryearna@vcu.edu>
2018-04-28 17:05:13 -0400

    Started creating a DBItemsHelper unit test, but it keeps failing.

--- app/src/test/java/com/example/usp05/githubtry/data_model/DBItemsHelperTest.java
+++ app/src/test/java/com/example/usp05/githubtry/data_model/DBItemsHelperTest.java
@@ -13,6 +13,7 @@ import org.junit.runner.RunWith;
 
 import java.lang.reflect.Method;
 import java.text.ParseException;
+import java.util.Calendar;
 import java.util.Collection;
 import java.util.Date;
 
@@ -47,56 +48,36 @@ import static org.junit.Assert.*;
  * Created by nathan on 4/28/18.
  */
 @RunWith(MockitoJUnitRunner.class)
-public class DBItemsHelperTest {
+public abstract class DBItemsHelperTest extends DBItemsHelper{
 
-    @Mock
-    Context mMockContext;
-
-
-    String username = "testUsername";
-
-    @Mock
-    User user = new User(username, null, null, null, null, null);
-
-    private static final Object[] EMPTY = {};
-
-    // Reflection is used to get private field/method access in certain classes (User class in this case)
-//    public void reflect() throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-//
-//        // Print all the method names & execution result
-//        Method methods[] = userClass.getDeclaredMethods();
-//        System.out.println("Access all the methods");
-//        for (Method method : methods) {
-//            System.out.println("Method Name: " + method.getName());
-//            System.out.println("Return type: " + method.getReturnType());
-//            method.setAccessible(true);
-//            System.out.println(method.invoke(user, EMPTY) + "\n");
-//        }
-//
-//        // Print all the field names & values
-//        Field fields[] = userClass.getDeclaredFields();
-//        System.out.println("Access all the fields");
-//        for (Field field : fields) {
-//            System.out.println("Field Name: " + field.getName());
-//            field.setAccessible(true);
-//            System.out.println(field.get(user) + "\n");
-//        }
-//    }
-
+    static String username = "testUsername";
+    static String dbName = "TEST_DATABASE";
     static UserHandler UHT;
-//    static User testUser;
+    static DBItemsHelper DBH;
 
-//    User user = new User();
-//    Class<?> userClass = user.getClass();
+    @Mock
+    static User user = new User(username, null, null, null, null, null);
+
+    @Mock
+    static Context mMockContext;
+
+    public DBItemsHelperTest() {
+        super(mMockContext);
+    }
 
     @BeforeClass
-    public static void setUpUserForTesting() throws Exception {
-//        DBItemsHelperTest userHack = new DBItemsHelperTest();
-//        userHack.reflect();
+    public static void setUpDBItemsHelperTest() throws Exception {
+        // User must be set up before setting up database
+        setUpUserForTest();
+        setUpDatabaseForTest();
+    }
 
+    static void setUpUserForTest() {
+        UHT = UserHandler.getInstance(user);
+    }
 
-//        testUser.setUsername(username);
-//        UHT = UserHandler.getInstance(testUser);
+    static void setUpDatabaseForTest() {
+        DBH = new DBItemsHelper(mMockContext);
     }
 
     @Before
@@ -104,109 +85,95 @@ public class DBItemsHelperTest {
         UHT = UserHandler.getInstance();
     }
 
-    @After
-    public void tearDown() throws Exception {
-    }
+    String itemName = "Item Name";
+    String itemType = "Item Type";
+    String itemLocation = "Item Location";
+    int itemQuantity = 50;
+    Date itemPurchaseDate = Calendar.getInstance().getTime();
+    Date itemExpirationDate = Calendar.getInstance().getTime();
+    String itemNotes = "Item Notes";
 
-    @Test
-    public void onCreate() throws Exception {
-        assertEquals(username, UHT.getUsername());
-    }
-
-    @Test
-    public void onUpgrade() throws Exception {
-    }
 
     @Test
     public void insertItem() throws Exception {
+        Item item = new Item();
+
+        item.setName(itemName);
+        item.setType(itemType);
+        item.setUsername(username);
+        item.setPurchaseDate(itemPurchaseDate);
+        item.setExpirationDate(itemExpirationDate);
+        item.setQuantity(itemQuantity);
+        item.setLocation(itemLocation);
+        item.setNotes(itemNotes);
+
+        DBH.insertItem(item);
+
+        boolean itemFound = false;
+        Cursor cursor = DBH.getItems(username);
+        if(cursor.moveToFirst()){
+            do {
+                String dbItemName = cursor.getString(cursor.getColumnIndex(DBH.ITEM_COL_NAME));
+                if(dbItemName.equals(itemName)) {
+                    String dbItemType = cursor.getString(cursor.getColumnIndex(DBH.ITEM_COL_TYPE));
+                    if(dbItemType.equals(itemType)) {
+                        int dbItemQty = cursor.getInt(cursor.getColumnIndex(DBH.ITEM_COL_QUANTITY));
+                        if (dbItemQty == itemQuantity) itemFound = true;
+                    }
+                }
+            } while(cursor.moveToNext());
+        } else {
+            fail("No items exist in database with test username.");
+        }
+        assertTrue(itemFound);
     }
 
-    @Test
-    public void insertedToast() throws Exception {
-    }
-
-    @Test
-    public void searchItem() throws Exception {
-    }
-
-    @Test
-    public void deleteItem() throws Exception {
-    }
-
-    @Test
-    public void editItem() throws Exception {
-    }
-
-    @Test
-    public void getItems() throws Exception {
-    }
-
-    @Test
-    public void getItems1() throws Exception {
-    }
-
-    @Test
-    public void getItemLocationDetails() throws Exception {
-    }
-
-    @Test
-    public void getFilteredItems() throws Exception {
-    }
-
-    @Test
-    public void getTypes() throws Exception {
-    }
-
-    @Test
-    public void getLocations() throws Exception {
-    }
-
-    @Test
-    public void getUniqueFilteredItems() throws Exception {
-    }
-
-    @Test
-    public void getTotalQuantity() throws Exception {
-    }
-
-    @Test
-    public void getDatabaseName() throws Exception {
-    }
-
-    @Test
-    public void setWriteAheadLoggingEnabled() throws Exception {
-    }
-
-    @Test
-    public void getWritableDatabase() throws Exception {
-    }
-
-    @Test
-    public void getReadableDatabase() throws Exception {
-    }
-
-    @Test
-    public void close() throws Exception {
-    }
-
-    @Test
-    public void onConfigure() throws Exception {
-    }
-
-    @Test
-    public void onCreate1() throws Exception {
-    }
-
-    @Test
-    public void onUpgrade1() throws Exception {
-    }
-
-    @Test
-    public void onDowngrade() throws Exception {
-    }
-
-    @Test
-    public void onOpen() throws Exception {
-    }
+//    @Test
+//    public void test_insertedToast() throws Exception {
+//    }
+//
+//    @Test
+//    public void test_searchItem() throws Exception {
+//    }
+//
+//    @Test
+//    public void test_deleteItem() throws Exception {
+//    }
+//
+//    @Test
+//    public void test_editItem() throws Exception {
+//    }
+//
+//    @Test
+//    public void test_getItems() throws Exception {
+//    }
+//
+//    @Test
+//    public void test_getItems1() throws Exception {
+//    }
+//
+//    @Test
+//    public void test_getItemLocationDetails() throws Exception {
+//    }
+//
+//    @Test
+//    public void test_getFilteredItems() throws Exception {
+//    }
+//
+//    @Test
+//    public void test_getTypes() throws Exception {
+//    }
+//
+//    @Test
+//    public void test_getLocations() throws Exception {
+//    }
+//
+//    @Test
+//    public void test_getUniqueFilteredItems() throws Exception {
+//    }
+//
+//    @Test
+//    public void test_getTotalQuantity() throws Exception {
+//    }
 
 }
